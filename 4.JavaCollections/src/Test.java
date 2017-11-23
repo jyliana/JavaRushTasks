f338a321553e344e2cb8168b64c94f1dfe294daf
Jyliana <jyliana@gmail.com>
2017-11-23 17:13:33 +0200

    arvhives

--- 4.JavaCollections/src/Test.java
+++ 4.JavaCollections/src/Test.java
@@ -1,23 +1,27 @@
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
-/**
- * Created by Inna on 11/20/17.
- */
+
 public class Test {
     public static void main(String[] args) throws IOException {
 
-        URL url = new URL("https://www.google.com.ua/images/srpr/logo11w.png");
-        InputStream inputStream = url.openStream();
+        FileOutputStream zipFile = new FileOutputStream("d:/archive.zip");
+        ZipOutputStream zip = new ZipOutputStream(zipFile);
 
-        Path tempFile = Files.createTempFile("temp-",".tmp");
-        /*Files.copy(inputStream, tempFile);*/
-        Files.copy(inputStream,tempFile, StandardCopyOption.REPLACE_EXISTING);
+        zip.putNextEntry(new ZipEntry("document.txt"));
+        File file = new File("d:/document-for-archive.txt");
+        Files.copy(file.toPath(), zip);
+        zip.closeEntry();
+
+        zip.putNextEntry(new ZipEntry("document2.txt"));
+        File file2 = new File("d:/document-for-archive2.txt");
+        Files.copy(file2.toPath(), zip);
+
+        zip.close();
 
     }
 }
---
+++ 4.JavaCollections/src/com/javarush/task/task31/task3105/Solution.java
@@ -0,0 +1,50 @@
+package com.javarush.task.task31.task3105;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
+
+/* 
+Добавление файла в архив
+*/
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        Path fileName = Paths.get(args[0]);
+        ZipInputStream inputZip = new ZipInputStream(new FileInputStream(args[1]));
+        ZipOutputStream outputZip = new ZipOutputStream(new FileOutputStream(args[1]));
+
+        Map<String, ByteArrayOutputStream> map = new HashMap<>();
+        ZipEntry entry;
+        while ((entry = inputZip.getNextEntry()) != null) {
+            if (entry.getName().equals("new/" + fileName.getFileName()))
+                continue;
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            byte[] buffer = new byte[1024];
+            int count;
+            while ((count = inputZip.read(buffer)) != -1)
+                byteArrayOutputStream.write(buffer, 0, count);
+            map.put(entry.getName(), byteArrayOutputStream);
+        }
+        inputZip.close();
+
+
+        for (Map.Entry<String, ByteArrayOutputStream> line : map.entrySet()) {
+            entry = new ZipEntry(line.getKey());
+            outputZip.putNextEntry(entry);
+            outputZip.write(line.getValue().toByteArray());
+        }
+        outputZip.putNextEntry(new ZipEntry("new/" + fileName.getFileName()));
+        Files.copy(fileName, outputZip);
+        outputZip.close();
+
+    }
+}
