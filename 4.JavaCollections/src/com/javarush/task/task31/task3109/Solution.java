4a1d2bb68a45b717acb29a8b7bd43c780f62ecbc
Jyliana <jyliana@gmail.com>
2017-11-24 16:05:26 +0200

    properties

---
+++ 4.JavaCollections/src/com/javarush/task/task31/task3109/Solution.java
@@ -0,0 +1,43 @@
+package com.javarush.task.task31.task3109;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
+
+/* 
+Читаем конфиги
+*/
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        Properties properties = solution.getProperties("4.JavaCollections/src/com/javarush/task/task31/task3109/properties.xml");
+        properties.list(System.out);
+
+        properties = solution.getProperties("4.JavaCollections/src/com/javarush/task/task31/task3109/properties.txt");
+        properties.list(System.out);
+
+        properties = solution.getProperties("4.JavaCollections/src/com/javarush/task/task31/task3109/notExists");
+        properties.list(System.out);
+    }
+
+    public Properties getProperties(String fileName) {
+        Properties properties = new Properties();
+        Path file = Paths.get(fileName);
+
+        try {
+            InputStream inStream = new FileInputStream(file.toAbsolutePath() + File.separator);
+            if (fileName.endsWith(".xml"))
+                properties.loadFromXML(inStream);
+            else
+                properties.load(inStream);
+
+        } catch (IOException e) {
+            return properties;
+        }
+        return properties;
+    }
+}
