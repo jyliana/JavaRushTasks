4948b2224053a9b55358320caebdc8d9c9a93c90
Jyliana <jyliana@gmail.com>
2017-11-29 22:40:54 +0200

    task2202 done

---
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2202/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.task.task22.task2202;
+
+/* 
+Найти подстроку
+*/
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(getPartOfString("JavaRush - лучший сервис обучения Java."));
+    }
+
+   /* public static String getPartOfString(String string) {
+
+        String[] tmp_line = string.split(" ");
+        String result = "";
+        if (string == null || string.isEmpty() || tmp_line.length < 5) {
+            throw new TooShortStringException();
+        }
+
+        for (int i = 1; i < 5; i++) {
+            result += tmp_line[i] + " ";
+        }
+        return result.trim();
+    }*/
+
+    public static String getPartOfString(String string) {
+
+        if (string == null || string.isEmpty())
+            throw new TooShortStringException();
+
+        String s[] = string.split(" ");
+        if (s.length < 5)
+            throw new TooShortStringException();
+
+        return s[1] + " " + s[2] + " " + s[3] + " " + s[4];
+    }
+
+    public static class TooShortStringException extends RuntimeException {
+    }
+}
