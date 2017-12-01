9fe69665286e40732b177766ada2455081510921
Jyliana <jyliana@gmail.com>
2017-12-02 00:37:07 +0200

    task2211 done

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2211/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2211/Solution.java
@@ -1,11 +1,27 @@
 package com.javarush.task.task22.task2211;
 
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.Charset;
 
 /* 
 Смена кодировки
 */
 public class Solution {
     public static void main(String[] args) throws IOException {
+        InputStream inStream = new FileInputStream(args[0]);
+        OutputStream outStream = new FileOutputStream(args[1]);
+
+        while (inStream.available() > 0) {
+            Charset utf8 = Charset.forName("UTF-8");
+            Charset windows1251 = Charset.forName("Windows-1251");
+
+            byte[] buffer = new byte[1000];
+            inStream.read(buffer);
+            String s = new String(buffer, windows1251);
+            buffer = s.getBytes(utf8);
+            outStream.write(buffer);
+        }
+        inStream.close();
+        outStream.close();
     }
 }
