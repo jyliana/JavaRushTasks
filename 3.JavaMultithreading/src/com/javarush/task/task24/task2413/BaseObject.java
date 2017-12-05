4f5290772b39138566c3944c3daf00e6d1c4af60
Jyliana <jyliana@gmail.com>
2017-12-05 15:28:19 +0200

    task2413 processing

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/BaseObject.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/BaseObject.java
@@ -1,6 +1,10 @@
 package com.javarush.task.task24.task2413;
 
-public class BaseObject {
+import java.awt.geom.Point2D;
+
+import static java.lang.Math.max;
+
+public abstract class BaseObject {
     private double x;
     private double y;
     private double radius;
@@ -34,4 +38,14 @@ public class BaseObject {
     public void setRadius(double radius) {
         this.radius = radius;
     }
+
+    public abstract void draw(Canvas canvas);
+
+    public abstract void move();
+
+    public boolean isIntersec(BaseObject o) {
+        double distance = Point2D.distance(this.getX(), this.getY(), o.getX(), o.getY());
+        return distance <= max(this.getRadius(), o.getRadius());
+    }
 }
+
--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
@@ -1,4 +1,67 @@
 package com.javarush.task.task24.task2413;
 
 public class Canvas {
+    private int width;
+    private int height;
+    private char[][] matrix;
+
+    public Canvas(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.matrix = new char[height + 2][width + 2];
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public char[][] getMatrix() {
+        return matrix;
+    }
+
+    public void setMatrix(char[][] matrix) {
+        this.matrix = matrix;
+    }
+
+    public void setPoint(double x, double y, char c) {
+        int x1 = (int) Math.round(x);
+        int y1 = (int) Math.round(y);
+        if (!(x < 0 || y < 0 || y > matrix.length || x > matrix[0].length)) {
+            matrix[y1][x1] = c;
+        }
+    }
+
+    public void drawMatrix(double x, double y, int[][] matrix, char c) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] != 0)
+                    setPoint(x + j, y + i, c);
+            }
+        }
+    }
+
+    public void clear() {
+        matrix = new char[height + 2][width + 2];
+    }
+
+    public void print() {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j]);
+            }
+            System.out.println();
+        }
+    }
 }
