a2c39f919ef96f25acb4e5c8171324b67a76c452
Jyliana <jyliana@gmail.com>
2017-12-07 16:53:41 +0200

    task3608 done

---
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task36.task3608;
+
+import com.javarush.task.task36.task3608.controller.Controller;
+import com.javarush.task.task36.task3608.model.MainModel;
+import com.javarush.task.task36.task3608.model.Model;
+import com.javarush.task.task36.task3608.view.EditUserView;
+import com.javarush.task.task36.task3608.view.UsersView;
+
+public class Solution {
+    public static void main(String[] args) {
+        Model model = new MainModel();
+        UsersView usersView = new UsersView();
+        Controller controller = new Controller();
+
+        EditUserView editUserView = new EditUserView();
+        controller.setEditUserView(editUserView);
+        editUserView.setController(controller);
+
+        usersView.setController(controller);
+        controller.setModel(model);
+        controller.setUsersView(usersView);
+
+        usersView.fireEventShowAllUsers();
+        usersView.fireEventOpenUserEditForm(126);
+        editUserView.fireEventUserDeleted(124);
+        editUserView.fireEventUserChanged("Tina", 126, 7);
+        usersView.fireEventShowDeletedUsers();
+    }
+}
