2868fbf84523820e909e170b791619f601b7c76c
Jyliana <jyliana@gmail.com>
2017-12-09 14:12:26 +0200

    task2508 done

---
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2508/TaskManipulator.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task25.task2508;
+
+public class TaskManipulator implements Runnable, CustomThreadManipulator {
+    private Thread thread;
+
+    @Override
+    public void run() {
+        try {
+            while (!thread.isInterrupted()) {
+                System.out.println(thread.getName());
+                Thread.sleep(100);
+            }
+        } catch (InterruptedException e) {
+
+        }
+    }
+
+    @Override
+    public void start(String threadName) {
+        thread = new Thread(this, threadName);
+        thread.start();
+    }
+
+    @Override
+    public void stop() {
+        thread.interrupt();
+    }
+}
