f4be3e6b28d919edf5030e95c6c478b7cd47017c
Jyliana <jyliana@gmail.com>
2017-12-14 23:31:07 +0200

    task2603 done

--- 3.JavaMultithreading/src/com/javarush/task/task26/task2603/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task26/task2603/Solution.java
@@ -8,7 +8,38 @@ import java.util.Comparator;
 public class Solution {
 
     public static void main(String[] args) {
+       /* List<Man> man = new ArrayList<>();
+        man.add(new Man("Бешкек", "Абдурасул", 13));
+        man.add(new Man("Анатолий", "Вассерман", 77));
+        man.add(new Man("Анатолий", "Булкин", 90));
+        man.add(new Man("Василий", "Теркин", 7));
+        man.add(new Man("Анатолий", "Вассерман", 64));
 
+        Comparator<Man> sortByName = new Comparator<Man>() {
+            @Override
+            public int compare(Man o1, Man o2) {
+                return o1.name.compareTo(o2.name);
+            }
+        };
+
+        Comparator<Man> sortBySurname = new Comparator<Man>() {
+            @Override
+            public int compare(Man o1, Man o2) {
+                return o1.surname.compareTo(o2.surname);
+            }
+        };
+        Comparator<Man> sortByAge = new Comparator<Man>() {
+            @Override
+            public int compare(Man o1, Man o2) {
+                return o1.age - o2.age;
+            }
+        };
+
+        Collections.sort(man, new CustomizedComparator<Man>(sortByName, sortBySurname, sortByAge));
+
+        for (Man item : man) {
+            System.out.println(item.name + " " + item.surname + " " + item.age);
+        }*/
     }
 
     public static class CustomizedComparator<T> implements Comparator<T> {
@@ -29,4 +60,16 @@ public class Solution {
             return result;
         }
     }
+
+   /* static class Man {
+        String name;
+        String surname;
+        Integer age;
+
+        public Man(String name, String surname, Integer age) {
+            this.name = name;
+            this.surname = surname;
+            this.age = age;
+        }
+    }*/
 }
