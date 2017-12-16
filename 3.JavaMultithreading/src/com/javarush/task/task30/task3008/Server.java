9d71e3f326df121125adfececd95c5f01f8d5042
Jyliana <jyliana@gmail.com>
2017-12-16 17:29:37 +0200

    task3008 processing

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -1,7 +1,28 @@
 package com.javarush.task.task30.task3008;
 
-public class Server {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class Server {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(ConsoleHelper.readInt());
+        ConsoleHelper.writeMessage("Server was started.");
+        try {
+            while (true)
+                new Handler(serverSocket.accept()).start();
+        } catch (Exception e) {
+            System.out.println("Something wrong.");
+        } finally {
+            serverSocket.close();
+        }
+    }
+
+    private static class Handler extends Thread {
+        private Socket socket;
+
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
     }
 }
