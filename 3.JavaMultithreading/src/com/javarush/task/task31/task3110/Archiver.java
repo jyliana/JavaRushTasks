52eccaa05609450289392cd2cf6d1a81280a13a6
Jyliana <jyliana@gmail.com>
2017-12-23 21:58:41 +0200

    task3110 processing

---
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task31.task3110;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.file.Paths;
+
+public class Archiver {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Please enter full path to an archive file.");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String archive = reader.readLine();
+
+        ZipFileManager zipFileManager = new ZipFileManager(Paths.get(archive));
+
+        System.out.println("Please enter full path to an file which will be archived.");
+        String newFile = reader.readLine();
+        zipFileManager.createZip(Paths.get(newFile));
+    }
+}
