696f3f026c622a853e5459bc0cc0c97670dc5648
Jyliana <jyliana@gmail.com>
2017-12-24 23:25:09 +0200

    task3110 processing

---
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ConsoleHelper.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task31.task3110;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ConsoleHelper {
+    private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String readString() {
+        while (true) {
+            try {
+                return reader.readLine();
+            } catch (IOException e) {
+                writeMessage("Произошла ошибка при попытке ввода текста. Попробуйте еще раз.");
+            }
+        }
+    }
+
+    public static int readInt() {
+        while (true) {
+            try {
+                return Integer.parseInt(readString());
+            } catch (NumberFormatException e) {
+                writeMessage("Произошла ошибка при попытке ввода числа. Попробуйте еще раз.");
+            }
+        }
+    }
+}
