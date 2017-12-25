97e44adf165a8a570035605eceef4640ac57b061
Jyliana <jyliana@gmail.com>
2017-12-25 16:16:15 +0200

    task3110 processing

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -1,14 +1,12 @@
 package com.javarush.task.task31.task3110;
 
-import com.javarush.task.task31.task3110.command.ExitCommand;
+import com.javarush.task.task31.task3110.exception.WrongZipFileException;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.nio.file.Paths;
+import java.io.IOException;
 
 public class Archiver {
     public static void main(String[] args) throws Exception {
-        System.out.println("Please enter full path to an archive file.");
+        /*System.out.println("Please enter full path to an archive file.");
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String archive = reader.readLine();
 
@@ -19,6 +17,32 @@ public class Archiver {
         zipFileManager.createZip(Paths.get(newFile));
 
         ExitCommand exitCommand = new ExitCommand();
-        exitCommand.execute();
+        exitCommand.execute();*/
+
+        Operation operation;
+        while (true) {
+            try {
+                operation = askOperation();
+                CommandExecutor.execute(operation);
+                if (operation == Operation.EXIT)
+                    break;
+            } catch (WrongZipFileException e) {
+                ConsoleHelper.writeMessage("Вы не выбрали файл архива или выбрали неверный файл.");
+            } catch (Exception e) {
+                ConsoleHelper.writeMessage("Произошла ошибка. Проверьте введенные данные.");
+            }
+        }
+    }
+
+    public static Operation askOperation() throws IOException {
+        ConsoleHelper.writeMessage("Выберите операцию:\n" +
+                "0 - упаковать файлы в архив\n" +
+                "1 - добавить файл в архив\n" +
+                "2 - удалить файл из архива\n" +
+                "3 - распаковать архив\n" +
+                "4 - просмотреть содержимое архива\n" +
+                "5 – выход");
+        int choose = ConsoleHelper.readInt();
+        return Operation.values()[choose];
     }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/CommandExecutor.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/CommandExecutor.java
@@ -9,12 +9,12 @@ public class CommandExecutor {
     private static final Map<Operation, Command> allKnownCommandsMap = new HashMap<>();
 
     static {
-        allKnownCommandsMap.put(Operation.ADD, new ZipAddCommand());
-        allKnownCommandsMap.put(Operation.CONTENT, new ZipContentCommand());
         allKnownCommandsMap.put(Operation.CREATE, new ZipCreateCommand());
-        allKnownCommandsMap.put(Operation.EXIT, new ExitCommand());
-        allKnownCommandsMap.put(Operation.EXTRACT, new ZipExtractCommand());
+        allKnownCommandsMap.put(Operation.ADD, new ZipAddCommand());
         allKnownCommandsMap.put(Operation.REMOVE, new ZipRemoveCommand());
+        allKnownCommandsMap.put(Operation.EXTRACT, new ZipExtractCommand());
+        allKnownCommandsMap.put(Operation.CONTENT, new ZipContentCommand());
+        allKnownCommandsMap.put(Operation.EXIT, new ExitCommand());
     }
 
     private CommandExecutor() {
