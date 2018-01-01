5c87f81be6ec1710c0ef4feca7bd25272191c1a4
Jyliana <jyliana@gmail.com>
2018-01-01 21:02:51 +0200

    task2805 done

---
+++ 3.JavaMultithreading/src/com/javarush/task/task28/task2805/MyThread.java
@@ -0,0 +1,42 @@
+package com.javarush.task.task28.task2805;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class MyThread extends Thread {
+    public static AtomicInteger counter = new AtomicInteger(1);
+
+    {
+        setPriority((counter.get() < 10) ? counter.getAndAdd(1) : counter.getAndAdd(-9));
+    }
+
+    public MyThread() {
+    }
+
+    public MyThread(Runnable target) {
+        super(target);
+    }
+
+    public MyThread(ThreadGroup group, Runnable target) {
+        super(group, target);
+    }
+
+    public MyThread(String name) {
+        super(name);
+    }
+
+    public MyThread(ThreadGroup group, String name) {
+        super(group, name);
+    }
+
+    public MyThread(Runnable target, String name) {
+        super(target, name);
+    }
+
+    public MyThread(ThreadGroup group, Runnable target, String name) {
+        super(group, target, name);
+    }
+
+    public MyThread(ThreadGroup group, Runnable target, String name, long stackSize) {
+        super(group, target, name, stackSize);
+    }
+}
