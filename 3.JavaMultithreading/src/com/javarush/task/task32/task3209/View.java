3d60aaba23916be0168594ae862e0b41273dc1f9
Jyliana <jyliana@gmail.com>
2018-01-06 23:39:30 +0200

    task3209 processing

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -120,4 +120,17 @@ public class View extends JFrame implements ActionListener {
     public boolean isHtmlTabSelected() {
         return tabbedPane.getSelectedIndex() == 0;
     }
+
+    public void selectHtmlTab() {
+        tabbedPane.setSelectedIndex(0);
+        resetUndo();
+    }
+
+    public void update() {
+        htmlTextPane.setDocument(controller.getDocument());
+    }
+
+    public void showAbout() {
+        JOptionPane.showMessageDialog(getContentPane(), "Первая программа с GUI", "Version 1.0", JOptionPane.INFORMATION_MESSAGE);
+    }
 }
