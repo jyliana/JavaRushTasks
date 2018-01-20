2421e25b814c7870ef95d27ac15dfc24ec303568
Jyliana <jyliana@gmail.com>
2018-01-20 15:36:12 +0200

    task2712 processing

---
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/statistic/StatisticManager.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task27.task2712.statistic;
+
+import com.javarush.task.task27.task2712.statistic.event.EventDataRow;
+
+public class StatisticManager {
+
+    private static StatisticManager instance = new StatisticManager();
+
+    private StatisticManager() {
+    }
+
+    public static StatisticManager getInstance() {
+        return instance;
+    }
+
+    public void register(EventDataRow data) {
+
+    }
+}
