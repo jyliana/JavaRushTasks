db929264779a249c232e0f40d98ce0ead03853c8
Jyliana <jyliana@gmail.com>
2018-02-16 00:02:41 +0200

    task3208 done

---
+++ 4.JavaCollections/src/com/javarush/task/task32/task3208/Solution.java
@@ -0,0 +1,60 @@
+package com.javarush.task.task32.task3208;
+
+import java.rmi.NotBoundException;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+/* 
+RMI-2
+*/
+public class Solution {
+    public static Registry registry;
+
+    //pretend we start rmi client as CLIENT_THREAD thread
+    public static Thread CLIENT_THREAD = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            try {
+                for (String bindingName : registry.list()) {
+                    Animal service = (Animal) registry.lookup(bindingName);
+                    service.printName();
+                    service.say();
+                }
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            } catch (NotBoundException e) {
+                e.printStackTrace();
+            }
+        }
+    });
+
+    //pretend we start rmi server as SERVER_THREAD thread
+    public static Thread SERVER_THREAD = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            //напишите тут ваш код
+            try {
+                registry = LocateRegistry.createRegistry(2099);
+                Cat cat = new Cat("Amanda");
+                Dog dog = new Dog("Rex");
+                Remote stub = UnicastRemoteObject.exportObject(cat, 0);
+                registry.bind("meow", stub);
+                stub = UnicastRemoteObject.exportObject(dog, 0);
+                registry.bind("woof", stub);
+            } catch (Exception e) {
+                System.err.print(e);
+            }
+        }
+    });
+
+    public static void main(String[] args) throws InterruptedException {
+        //start rmi server thread
+        SERVER_THREAD.start();
+        Thread.sleep(1000);
+        //start client
+        CLIENT_THREAD.start();
+    }
+}
