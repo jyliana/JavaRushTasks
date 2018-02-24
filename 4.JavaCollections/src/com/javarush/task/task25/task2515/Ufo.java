14d1df26bd8b3964da00a1d9c0ed73a63e8a12e9
Jyliana <jyliana@gmail.com>
2018-02-24 15:58:39 +0200

    task2515 is processing

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Ufo.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Ufo.java
@@ -1,7 +1,35 @@
 package com.javarush.task.task25.task2515;
 
+import java.util.Random;
+
 public class Ufo extends BaseObject {
-    public Ufo(double x, double y, double radius) {
-        super(x, y, radius);
+    private static int[][] matrix = {
+            {0, 0, 0, 0, 0},
+            {0, 0, 1, 0, 0},
+            {0, 0, 1, 0, 0},
+            {1, 0, 1, 0, 1},
+            {1, 1, 1, 1, 1},
+    };
+
+    public Ufo(double x, double y) {
+        super(x, y, 3);
+    }
+
+    public void draw(Canvas canvas) {
+        canvas.drawMatrix(x - radius + 1, y - radius + 1, matrix, 'U');
+    }
+
+    public void move() {
+        double dx = Math.random() * 2 - 1;
+        double dy = Math.random() * 2 - 1;
+        x += dx;
+        y += dy;
+        checkBorders(radius, Space.game.getWidth() - radius, radius, Space.game.getHeight() / 2);
+        if (new Random().nextInt(10) == 1)
+            fire();
+    }
+
+    public void fire() {
+        Space.game.getBombs().add(new Bomb(x, y + 3));
     }
 }
