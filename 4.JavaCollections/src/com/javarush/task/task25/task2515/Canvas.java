fb30eeb1ebe8ade03969319bafd79120e3ffd246
Jyliana <jyliana@gmail.com>
2018-02-24 13:36:41 +0200

    task2515 is processing

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Canvas.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Canvas.java
@@ -1,4 +1,41 @@
 package com.javarush.task.task25.task2515;
 
 public class Canvas {
+    private int width;
+    private int height;
+    private char[][] matrix;
+
+    public Canvas(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.matrix = new char[height][width];
+    }
+
+    public int getWidth() {
+
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public char[][] getMatrix() {
+        return matrix;
+    }
+
+    public void setPoint(double x, double y, char c) {
+        if (!(x < 0 || y < 0 || y >= matrix.length || x >= matrix[0].length)) {
+            matrix[(int) Math.round(y)][(int) Math.round(x)] = c;
+        }
+    }
+
+    public void drawMatrix(double x, double y, int[][] matrix, char c) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] != 0)
+                    setPoint(x + j, y + i, c);
+            }
+        }
+    }
 }
