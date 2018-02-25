0c0e9e7651de05e75d07b96c8d57da12fd5aa82e
Jyliana <jyliana@gmail.com>
2018-02-25 15:06:37 +0200

    task3402 done

---
+++ 4.JavaCollections/src/com/javarush/task/task34/task3402/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task34.task3402;
+
+/*
+Факториал с помощью рекурсии
+*/
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        System.out.println(solution.factorial(9));     //362880
+        System.out.println(solution.factorial(0));     //1
+        System.out.println(solution.factorial(1));     //1
+    }
+
+    public int factorial(int n) {
+        int result;
+
+        if (n == 1 || n == 0) return 1;
+        result = factorial(n - 1) * n;
+        return result;
+    }
+}
