563dac9a3fd2d4406f781c9f85ea6f3bacd8dc7e
Jyliana <jyliana@gmail.com>
2018-03-12 21:39:42 +0200

    task3702 is processed

--- 4.JavaCollections/src/com/javarush/task/task37/task3702/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3702/Solution.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task37.task3702;
 
+import com.javarush.task.task37.task3702.female.FemaleFactory;
 import com.javarush.task.task37.task3702.male.MaleFactory;
 
 public class Solution {
@@ -8,5 +9,10 @@ public class Solution {
         System.out.println(factory.getPerson(99));
         System.out.println(factory.getPerson(4));
         System.out.println(factory.getPerson(15));
+
+        FemaleFactory factory2 = new FemaleFactory();
+        System.out.println(factory2.getPerson(99));
+        System.out.println(factory2.getPerson(4));
+        System.out.println(factory2.getPerson(15));
     }
 }
