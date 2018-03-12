5945c9b293195bfc0a4b6fea1689d56b5292ccfd
Jyliana <jyliana@gmail.com>
2018-03-12 21:26:50 +0200

    task3702 is processed

--- 4.JavaCollections/src/com/javarush/task/task37/task3702/male/TeenBoy.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3702/male/TeenBoy.java
@@ -3,7 +3,7 @@ package com.javarush.task.task37.task3702.male;
 import com.javarush.task.task37.task3702.Human;
 
 public class TeenBoy implements Human {
-    public static final int MAX_AGE = 12;
+    public static final int MAX_AGE = 19;
 
     @Override
     public String toString() {
