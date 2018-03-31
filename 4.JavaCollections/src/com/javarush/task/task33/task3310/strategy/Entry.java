d9978172e95d8ea3a1b58c83dd387ef0fb81d02e
Jyliana <jyliana@gmail.com>
2018-04-01 01:15:44 +0300

    task3310 processing

---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/strategy/Entry.java
@@ -0,0 +1,49 @@
+package com.javarush.task.task33.task3310.strategy;
+
+import java.io.Serializable;
+
+public class Entry implements Serializable {
+    int hash;
+    Long key;
+    String value;
+    Entry next;
+
+    public Entry(int hash, Long key, String value, Entry next) {
+        this.hash = hash;
+        this.key = key;
+        this.value = value;
+        this.next = next;
+    }
+
+    public Long getKey() {
+        return key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Entry entry = (Entry) o;
+
+        if (key != null ? !key.equals(entry.key) : entry.key != null) return false;
+        return value != null ? value.equals(entry.value) : entry.value == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = key != null ? key.hashCode() : 0;
+        result = 31 * result + (value != null ? value.hashCode() : 0);
+        return result;
+    }
+
+
+    @Override
+    public String toString() {
+        return new String(key + "=" + value);
+    }
+}
