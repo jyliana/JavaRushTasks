1169b0ed06ebc1d2904a6b6daee917e8d0dee5f2
Jyliana <jyliana@gmail.com>
2018-03-31 14:05:50 +0300

    task3310 processing

--- 4.JavaCollections/src/com/javarush/task/task33/task3310/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/Solution.java
@@ -1,5 +1,7 @@
 package com.javarush.task.task33.task3310;
 
+import com.javarush.task.task33.task3310.strategy.HashMapStorageStrategy;
+
 public class Solution {
     public static void main(String[] args) {
 
