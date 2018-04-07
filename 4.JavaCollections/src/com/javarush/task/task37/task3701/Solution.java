7d83e5e2d750577138cfd349cdd4ea829e2e9788
Jyliana <jyliana@gmail.com>
2018-04-07 22:21:56 +0300

    task3701 done

---
+++ 4.JavaCollections/src/com/javarush/task/task37/task3701/Solution.java
@@ -0,0 +1,63 @@
+package com.javarush.task.task37.task3701;
+
+import java.util.ArrayList;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.function.Consumer;
+
+/* 
+Круговой итератор
+*/
+public class Solution<T> extends ArrayList<T> {
+    public static void main(String[] args) {
+        Solution<Integer> list = new Solution<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        int count = 0;
+        for (Integer i : list) {
+            //1 2 3 1 2 3 1 2 3 1
+            System.out.print(i + " ");
+            count++;
+            if (count == 10) {
+                break;
+            }
+        }
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new RoundIterator();
+    }
+
+    public class RoundIterator implements Iterator<T> {
+        Iterator<T> iterator = Solution.super.iterator();
+        int cursor = 0;
+
+        @Override
+        public boolean hasNext() {
+            if (cursor >= size()) {
+                iterator = Solution.super.iterator();
+                cursor = 0;
+            }
+            return size() != 0;
+        }
+
+        @Override
+        public T next() {
+            cursor++;
+            return iterator.next();
+        }
+
+        @Override
+        public void remove() throws ConcurrentModificationException {
+            iterator.remove();
+        }
+
+        @Override
+        public void forEachRemaining(Consumer<? super T> action) {
+            iterator.forEachRemaining(action);
+        }
+    }
+}
