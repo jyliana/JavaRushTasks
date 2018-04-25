b01df6ff011a41efb76be9381167b1c594828201
Jyliana <jyliana@gmail.com>
2018-04-25 23:25:55 +0300

    task2810 is processing

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/vo/Vacancy.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/vo/Vacancy.java
@@ -12,6 +12,33 @@ public class Vacancy {
         return title;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Vacancy)) return false;
+
+        Vacancy vacancy = (Vacancy) o;
+
+        if (title != null ? !title.equals(vacancy.title) : vacancy.title != null) return false;
+        if (salary != null ? !salary.equals(vacancy.salary) : vacancy.salary != null) return false;
+        if (city != null ? !city.equals(vacancy.city) : vacancy.city != null) return false;
+        if (companyName != null ? !companyName.equals(vacancy.companyName) : vacancy.companyName != null) return false;
+        if (siteName != null ? !siteName.equals(vacancy.siteName) : vacancy.siteName != null) return false;
+        return url != null ? url.equals(vacancy.url) : vacancy.url == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = title != null ? title.hashCode() : 0;
+        result = 31 * result + (salary != null ? salary.hashCode() : 0);
+        result = 31 * result + (city != null ? city.hashCode() : 0);
+        result = 31 * result + (companyName != null ? companyName.hashCode() : 0);
+        result = 31 * result + (siteName != null ? siteName.hashCode() : 0);
+        result = 31 * result + (url != null ? url.hashCode() : 0);
+        return result;
+    }
+
     public void setTitle(String title) {
         this.title = title;
     }
