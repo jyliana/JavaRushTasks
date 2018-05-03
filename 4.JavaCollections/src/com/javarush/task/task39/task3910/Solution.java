3f61fb7e26c0688256560f0b7cf8e3c64d1fc0eb
Jyliana <jyliana@gmail.com>
2018-05-03 23:19:39 +0300

    3910 done

---
+++ 4.JavaCollections/src/com/javarush/task/task39/task3910/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task39.task3910;
+
+/* 
+isPowerOfThree
+*/
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean isPowerOfThree(int n) {
+        if (n == 0) return
+                false;
+
+        while ((n % 3) == 0) {
+            n = n / 3;
+        }
+
+        if (n == 1)
+            return true;
+
+        return false;
+    }
+}
