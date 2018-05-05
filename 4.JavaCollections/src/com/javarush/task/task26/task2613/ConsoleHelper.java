0670f7171115be352e8dc2fe5c2815fd17cd2227
Jyliana <jyliana@gmail.com>
2018-05-05 16:07:03 +0300

    task2613(2) done

---
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/ConsoleHelper.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task26.task2613;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ConsoleHelper {
+    private static BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String readString() {
+        String line = "";
+        try {
+            line = bis.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return line;
+    }
+}
