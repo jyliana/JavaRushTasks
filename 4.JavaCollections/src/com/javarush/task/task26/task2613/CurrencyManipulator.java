d20f6e35bde69a31e3b82b425f09f947db5ee45d
Jyliana <jyliana@gmail.com>
2018-05-09 21:43:45 +0300

    task2613(10) done

--- 4.JavaCollections/src/com/javarush/task/task26/task2613/CurrencyManipulator.java
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/CurrencyManipulator.java
@@ -1,7 +1,8 @@
 package com.javarush.task.task26.task2613;
 
-import java.util.HashMap;
-import java.util.Map;
+import com.javarush.task.task26.task2613.exception.NotEnoughMoneyException;
+
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class CurrencyManipulator {
@@ -31,5 +32,60 @@ public class CurrencyManipulator {
         if (getTotalAmount() > 0)
             return true;
         return false;
+    }
+
+    public boolean isAmountAvailable(int expectedAmount) {
+        if (getTotalAmount() >= expectedAmount)
+            return true;
+        else return false;
+    }
+
+    public Map<Integer, Integer> withdrawAmount(int expectedAmount) throws NotEnoughMoneyException {
+        Map<Integer, Integer> copy = denominations.entrySet().stream()
+                .sorted(Map.Entry.comparingByKey(Comparator.reverseOrder()))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
+                        (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+
+        Map<Integer, Integer> copyRepeat = new HashMap<>();
+        copyRepeat.putAll(copy);
+        Map<Integer, Integer> result = new HashMap<>();
+        int tmpAmount = expectedAmount;
+
+        while (tmpAmount != 0 && copy.size() != 0) {
+            for (Integer element : copy.keySet()) {
+                if (element <= tmpAmount) {
+                    while (element <= tmpAmount && copy.get(element) != 0) {
+                        if (result.containsKey(element)) {
+                            result.put(element, result.get(element) + 1);
+                        } else {
+                            result.put(element, 1);
+                        }
+                        copy.put(element, copy.get(element) - 1);
+                        tmpAmount -= element;
+                    }
+                    if (tmpAmount == 0)
+                        break;
+                }
+            }
+            if (tmpAmount != 0) {
+                Optional<Map.Entry<Integer, Integer>> in = copy.entrySet().stream()
+                        .findFirst();
+                copyRepeat.remove(in.get().getKey());
+                copy.clear();
+                copy.putAll(copyRepeat);
+                tmpAmount = expectedAmount;
+                result = new HashMap<>();
+            }
+        }
+        if (tmpAmount != 0) {
+            throw new NotEnoughMoneyException();
+        }
+        for (Map.Entry item : copy.entrySet()) {
+            if ((int) item.getValue() == 0) {
+                denominations.remove(item.getKey());
+            } else if ((int) item.getValue() != denominations.get(item.getKey()))
+                denominations.put((int) item.getKey(), (int) item.getValue());
+        }
+        return result;
     }
 }
--- 4.JavaCollections/src/com/javarush/task/task26/task2613/command/WithdrawCommand.java
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/command/WithdrawCommand.java
@@ -1,8 +1,33 @@
 package com.javarush.task.task26.task2613.command;
 
+import com.javarush.task.task26.task2613.ConsoleHelper;
+import com.javarush.task.task26.task2613.CurrencyManipulator;
+import com.javarush.task.task26.task2613.CurrencyManipulatorFactory;
+import com.javarush.task.task26.task2613.exception.InterruptOperationException;
+import com.javarush.task.task26.task2613.exception.NotEnoughMoneyException;
+
+import java.util.Map;
+
 class WithdrawCommand implements Command {
     @Override
-    public void execute() {
-
+    public void execute() throws InterruptOperationException {
+        String code = ConsoleHelper.askCurrencyCode();
+        CurrencyManipulator currencyManipulator = CurrencyManipulatorFactory.getManipulatorByCurrencyCode(code);
+        while (true) {
+            ConsoleHelper.writeMessage("Please enter how much money you want to withdraw:");
+            String amount = ConsoleHelper.readString();
+            while (!amount.matches("^[1-9]\\d*$") || !currencyManipulator.isAmountAvailable(Integer.parseInt(amount))) {
+                ConsoleHelper.writeMessage("Incorrect data, please try again.");
+                amount = ConsoleHelper.readString();
+            }
+            try {
+                Map<Integer, Integer> result = currencyManipulator.withdrawAmount(Integer.parseInt(amount));
+                result.entrySet().stream()
+                        .forEach(n -> System.out.println(n.getKey() + " - " + n.getValue()));
+                break;
+            } catch (NotEnoughMoneyException e) {
+                System.out.println("Not enough money. Please try other amount of money.");
+            }
+        }
     }
 }
