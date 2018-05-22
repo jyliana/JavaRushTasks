89d3203e64a3b692c2f29ad101a85a1e8580678b
Jyliana <jyliana@gmail.com>
2018-05-22 23:56:57 +0300

    task3410(8) done

---
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/model/Wall.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task34.task3410.model;
+
+import java.awt.*;
+
+public class Wall extends CollisionObject {
+    public Wall(int x, int y) {
+        super(x, y);
+    }
+
+    @Override
+    public void draw(Graphics graphics) {
+        graphics.setColor(Color.LIGHT_GRAY);
+        int leftUpperCornerX = getX() - getWidth() / 2;
+        int leftUpperCornerY = getY() - getHeight() / 2;
+
+        graphics.drawRect(leftUpperCornerX, leftUpperCornerY, getWidth(), getHeight());
+        graphics.fillRect(leftUpperCornerX, leftUpperCornerY, getWidth(), getHeight());
+
+    }
+}
--- 4.JavaCollections/src/com/javarush/task/task34/task3410/view/Field.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/view/Field.java
@@ -3,6 +3,7 @@ package com.javarush.task.task34.task3410.view;
 import com.javarush.task.task34.task3410.model.Box;
 import com.javarush.task.task34.task3410.model.Home;
 import com.javarush.task.task34.task3410.model.Player;
+import com.javarush.task.task34.task3410.model.Wall;
 
 import javax.swing.*;
 import java.awt.*;
@@ -19,8 +20,10 @@ public class Field extends JPanel {
         Box box = new Box(14, 15);
         Player player = new Player(153, 158);
         Home home = new Home(200, 200);
+        Wall wall = new Wall(300, 300);
         box.draw(g);
         player.draw(g);
         home.draw(g);
+        wall.draw(g);
     }
 }
