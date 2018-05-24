4c994aa7123edb3ea165db312f037a10a5646fd0
Jyliana <jyliana@gmail.com>
2018-05-24 23:12:55 +0300

    task3410(14) done

--- 4.JavaCollections/src/com/javarush/task/task34/task3410/view/Field.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/view/Field.java
@@ -1,11 +1,16 @@
 package com.javarush.task.task34.task3410.view;
 
 import com.javarush.task.task34.task3410.controller.EventListener;
+import com.javarush.task.task34.task3410.model.Direction;
 import com.javarush.task.task34.task3410.model.GameObject;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.util.Set;
+
+import static java.awt.event.KeyEvent.*;
 
 
 public class Field extends JPanel {
@@ -14,6 +19,9 @@ public class Field extends JPanel {
 
     public Field(View view) {
         this.view = view;
+        KeyHandler keyHandler = new KeyHandler();
+        addKeyListener(keyHandler);
+        setFocusable(true);
     }
 
     public void paint(Graphics g) {
@@ -29,13 +37,36 @@ public class Field extends JPanel {
         g.fillRect(0, 0, 490, 510);
 
         Set<GameObject> objects = view.getGameObjects().getAll();
-        objects.stream().forEach(object->object.draw(g));
+        objects.stream().forEach(object -> object.draw(g));
 
 
     }
 
     public void setEventListener(EventListener eventListener) {
         this.eventListener = eventListener;
+    }
+
+    public class KeyHandler extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+            switch (e.getKeyCode()) {
+                case VK_LEFT:
+                    eventListener.move(Direction.LEFT);
+                    break;
+                case VK_RIGHT:
+                    eventListener.move(Direction.RIGHT);
+                    break;
+                case VK_UP:
+                    eventListener.move(Direction.UP);
+                    break;
+                case VK_DOWN:
+                    eventListener.move(Direction.DOWN);
+                    break;
+                case VK_R:
+                    eventListener.restart();
+                    break;
+            }
+        }
     }
 
 }
